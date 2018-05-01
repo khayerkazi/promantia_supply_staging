0fe332940d6d003588af13847e5f5abcbccf2dc7
Nitesh Soni <nitesh.soni@promantia.com>
2018-05-01 16:07:32 +0530

    AD changes for GoodsMovementsInvoice Report Print

---
+++ dependantModules/org.openbravo.warehouse.shipping/src/org/openbravo/warehouse/shipping/adreports/GoodsMovementsInvoicePrint.java
@@ -0,0 +1,9 @@
+package org.openbravo.warehouse.shipping.adreports;
+
+import org.openbravo.base.secureApp.HttpSecureAppServlet;
+
+public class GoodsMovementsInvoicePrint extends HttpSecureAppServlet {
+
+  private static final long serialVersionUID = 1L;
+
+}
