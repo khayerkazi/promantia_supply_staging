61aa19f61e13f2ba1149b3ceef6e35c91468d0ae
Nitesh Soni <nitesh.soni@promantia.com>
2018-07-05 18:28:36 +0530

    fixed shipping report issue for hsn

--- com.promantia.supply.gstcustomizations/src/com/promantia/supply/gstcustomizations/adreports/GsInvoiceReport.java
+++ com.promantia.supply.gstcustomizations/src/com/promantia/supply/gstcustomizations/adreports/GsInvoiceReport.java
@@ -8,13 +8,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-
-
-
-
-
-
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperReport;
@@ -23,6 +16,10 @@ import net.sf.jasperreports.engine.xml.JRXmlLoader;
 
 import org.openbravo.base.secureApp.HttpSecureAppServlet;
 import org.openbravo.base.secureApp.VariablesSecureApp;
+import org.openbravo.dal.service.OBDal;
+import org.openbravo.model.materialmgmt.transaction.ShipmentInOutLine;
+import org.openbravo.warehouse.shipping.OBWSHIPShipping;
+import org.openbravo.warehouse.shipping.OBWSHIPShippingDetails;
 
 public class GsInvoiceReport extends HttpSecureAppServlet {
   private static final long serialVersionUID = 1L;
@@ -43,28 +40,52 @@ public class GsInvoiceReport extends HttpSecureAppServlet {
       if (strShippingId.equals(""))
         strShippingId = vars.getSessionValue("GS_InvoiceReport.inpobwshipShippingId");
       // strcBpartnerId = vars.getSessionValue("PrintRfQ.inpcBpartnerId");
+
       printPagePartePDF(response, vars, strShippingId);
+
+      String trimmedShippingID = strShippingId.substring(2, strShippingId.length() - 2);
+      OBWSHIPShipping shippingObj = OBDal.getInstance().get(OBWSHIPShipping.class,
+          trimmedShippingID);
+
+      for (OBWSHIPShippingDetails shippinglineObj : shippingObj.getOBWSHIPShippingDetailsList()) {
+        if (shippinglineObj.getGoodsShipment() != null) {
+          for (ShipmentInOutLine inoutLineObj : shippinglineObj.getGoodsShipment()
+              .getMaterialMgmtShipmentInOutLineList()) {
+            if (inoutLineObj.getObwshipHsncode() == null) {
+              if (inoutLineObj.getProduct() != null) {
+                if (inoutLineObj.getProduct().getIngstGstproductcode() != null) {
+                  if (inoutLineObj.getProduct().getIngstGstproductcode().getValue() != null) {
+                    inoutLineObj.setObwshipHsncode(inoutLineObj.getProduct()
+                        .getIngstGstproductcode().getValue());
+                    OBDal.getInstance().save(inoutLineObj);
+                    OBDal.getInstance().flush();
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+
     } else
       pageError(response);
   }
 
   private void printPagePartePDF(HttpServletResponse response, VariablesSecureApp vars,
       String strShippingId) throws IOException, ServletException {
-    
-	  
-	  
-	  
+
     String strBaseDesign = getBaseDesignPath(vars.getLanguage());
     HashMap<String, Object> parameters = new HashMap<String, Object>();
 
     JasperReport jasperReportLines;
     try {
- /*JasperCompileManager.compileReportToFile(strBaseDesign
-                + "/com/promantia/supply/gstcustomizations/adreports/InvoiceRe.jrxml");
-*/
-     JasperDesign jasperDesignLines = JRXmlLoader.load(strBaseDesign
+      /*
+       * JasperCompileManager.compileReportToFile(strBaseDesign +
+       * "/com/promantia/supply/gstcustomizations/adreports/InvoiceRe.jrxml");
+       */
+      JasperDesign jasperDesignLines = JRXmlLoader.load(strBaseDesign
           + "/com/promantia/supply/gstcustomizations/adreports/InvoiceRe.jrxml");
-      jasperReportLines = JasperCompileManager.compileReport(jasperDesignLines); 
+      jasperReportLines = JasperCompileManager.compileReport(jasperDesignLines);
     } catch (JRException e) {
       e.printStackTrace();
       throw new ServletException(e.getMessage());
@@ -74,16 +95,11 @@ public class GsInvoiceReport extends HttpSecureAppServlet {
     parameters.put("BASE_DESIGN", strBaseDesign);
     parameters.put("DOCUMENT_ID", strShippingId);
     renderJR(vars, response, null, "pdf", parameters, null, null);
-    
-    
-    
-    
+
   }
 
   public String getServletInfo() {
     return "Servlet that presents the RptMRequisitions seeker";
   } // End of getServletInfo() method
-  
-  
-  
+
 }
