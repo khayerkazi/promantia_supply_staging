43ddb04e1d205f2e0cf6d2ab0e2437e0e02531bb
Gajanan <gajanan.shrinivas@promantia.com>
2019-10-12 12:31:50 +0530

    Get order api initial commit

---
+++ in.decathlon.ibud.masters/src/in/decathlon/ibud/masters/client/GetOrderDetailsFromProd.java
@@ -0,0 +1,13 @@
+package in.decathlon.ibud.masters.client;
+
+import org.openbravo.scheduling.Process;
+import org.openbravo.scheduling.ProcessBundle;
+
+public class GetOrderDetailsFromProd implements Process {
+
+  @Override
+  public void execute(ProcessBundle bundle) throws Exception {
+
+  }
+
+}
